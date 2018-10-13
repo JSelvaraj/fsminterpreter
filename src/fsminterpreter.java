3436877a9b3302194fa86779f14ce0c277b954c4
JSelvaraj <selvarajjeffrey@yahoo.co.uk>
2018-10-13 01:49:50 +0100

    First working program

--- src/fsminterpreter.java
+++ src/fsminterpreter.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.Hashtable;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -8,26 +7,30 @@ public class fsminterpreter {
 
     static LinkedList<String> setOfStates = new LinkedList<>();
     static LinkedList<String> setOfInputs = new LinkedList<>();
-    static Hashtable<String,String>[][] states;
+    static Hashtable<String, String>[][] states;
+    static String FILE_NAME = "bigger.fsm";
+    static int OUTPUT = 0;
+    static int NEXT_STATE = 1;
+    static String INPUT_FILE = "input.txt";
+
 
     public static void main(String args[]) {
-        getStatesAndInputs();
-        makeStates();
-        makeHashtables();
-
-
-
+        fsminterpreter FSM = new fsminterpreter();
+        FSM.getStatesAndInputs();
+        FSM.makeStates();
+        FSM.makeHashtables();
+        FSM.runFSM();
 
 
         printSetOfStates();
-        printSetOfInputs();
+//        printSetOfInputs();
 
     }
 
-    private static void getStatesAndInputs() {
+    private void getStatesAndInputs() {
         int position = 0;
         try {
-            FileReader fr = new FileReader("fsm.txt");
+            FileReader fr = new FileReader(FILE_NAME);
             Scanner column = new Scanner(fr);
             String currentToken;
             while (column.hasNext()) {
@@ -49,16 +52,17 @@ public class fsminterpreter {
         }
     }
 
-    private static void makeStates() {
+    private void makeStates() {
         states = new Hashtable[setOfStates.size()][2];
-        for (int i = 0; i< setOfStates.size();i++) {
+        for (int i = 0; i < setOfStates.size(); i++) {
             states[i][0] = new Hashtable<>();
             states[i][1] = new Hashtable<>();
         }
     }
-    private static void makeHashtables() {
+
+    private void makeHashtables() {
         try {
-            FileReader fr = new FileReader("fsm.txt");
+            FileReader fr = new FileReader(FILE_NAME);
             Scanner column = new Scanner(fr);
             String currentToken;
             int currentState = 0;
@@ -71,9 +75,10 @@ public class fsminterpreter {
                 } else if (position == 1) {
                     currentInput = currentToken;
                 } else if (position == 2) {
-                    states[currentState][0].put(currentInput,currentToken);
+                    states[currentState][OUTPUT].put(currentInput, currentToken);
                 } else if (position == 3) {
-                    states[currentState][1].put(currentInput,currentToken);
+                    validState(currentToken);
+                    states[currentState][NEXT_STATE].put(currentInput, currentToken);
                 }
                 position = (position + 1) % 4;
             }
@@ -85,16 +90,63 @@ public class fsminterpreter {
 
     }
 
+    private boolean validState(String state) {
+        if (!setOfStates.contains(state)) {
+            System.out.println("Bad description");
+            System.exit(0);
+        }
+        return true;
+    }
+
+    private void runFSM() {
+        try {
+            FileReader fr = new FileReader(INPUT_FILE);
+            BufferedReader reader = new BufferedReader(fr);
+            int currentState = 0;
+            int currentInput = reader.read();
+            while (currentInput != -1 && Character.isLetterOrDigit((char) currentInput)) {
+                validInput(currentInput);
+                System.out.print(getOutput(currentState, currentInput));
+                currentState = getNextState(currentState, currentInput) - 1;
+                currentInput = reader.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getOutput(int currentState, int currentInput) {
+        String inputKey = Character.toString((char) currentInput);
+        return states[currentState][OUTPUT].get(inputKey);
+    }
+
+    private int getNextState(int currentState, int currentInput) {
+        String nextStateString = states[currentState][NEXT_STATE].get(Character.toString((char) currentInput));
+        return Integer.parseInt(nextStateString);
 
 
+    }
 
-    private static void printSetOfStates() {
+    private void validInput(int currentinput) {
+        String character = Character.toString((char) currentinput);
+        if (!setOfInputs.contains(character)) {
+            System.out.println();
+            System.out.println("Bad input");
+            System.exit(1);
+        }
+    }
+
+
+    private void printSetOfStates() {
         for (String s : setOfStates) {
             System.out.print(s + " ");
         }
         System.out.println();
     }
-    private static void printSetOfInputs() {
+
+    private void printSetOfInputs() {
         for (String s : setOfInputs) {
             System.out.print(s + " ");
         }
---
+++ src/setOfInputs.java
@@ -0,0 +1,5 @@
+import java.util.LinkedList;
+
+public class setOfInputs {
+    private LinkedList<String>
+}
